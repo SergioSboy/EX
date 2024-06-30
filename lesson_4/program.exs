6455865f20077acbd5366181dc80410f7cf6537b
Sergio <seregaant71@gmail.com>
2024-06-30 16:44:12 +0300

    homework accept!

--- lesson_4/program.exs
+++ lesson_4/program.exs
@@ -2,7 +2,15 @@
 
 defmodule Demo do
   # keep_odd/1
-  def odd do
-    # ...
+  def odd([]), do: []
+  def odd([head | tail]) do
+    if rem(head, 2) == 1 do
+     [head | odd(tail)]
+    else
+      odd(tail)
+    end
   end
 end
+
+list = [1, 2, 3, 4, 5, 6]
+list |> Demo.odd() |> IO.inspect()
